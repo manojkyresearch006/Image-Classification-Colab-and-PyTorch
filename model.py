e0454f1599e3fce6c1929b17e6d21c29bbf1a4e9
manojkyresearch006 <74037021+manojkyresearch006@users.noreply.github.com>
2021-07-05 02:42:21 -0700

    adding changes to model

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-class ClassifyNet():
+class ClassifyNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1=nn.Conv2d(3,6,5)
@@ -18,4 +18,3 @@ class ClassifyNet():
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-        
