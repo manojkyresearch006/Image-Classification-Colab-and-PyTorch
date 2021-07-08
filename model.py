5c67433180f3000417743a68c0c42c61744af9cc
manojkyresearch006 <74037021+manojkyresearch006@users.noreply.github.com>
2021-07-08 04:24:16 -0700

    adding Batchnorm layers

--- model.py
+++ model.py
@@ -5,9 +5,13 @@ import torch
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(3, 8, 5)
+        self.norm1=nn.BatchNorm2d(8)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv2 = nn.Conv2d(8, 16, 5)
+        self.norm2=nn.BatchNorm2d(16)
+        self.conv3=nn.Conv2d(16,32,5)
+        self.norm3=nn.BatchNorm2d(32)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
